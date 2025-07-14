a601e3b1da2da30e56dd2330c350955b4e9e2b97
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-07-14 15:51:04 +0100

    refactor: slimmed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React from "react";
 import LandingPage from "./pages/LandingPage";
 import GalleryPage from "./pages/GalleryPage";
+import Header from "./components/Header";
 
+/**
+ * Top-level component
+ */
 const App: React.FC = () => {
-  // Simple page selector
-  const Page = window.location.pathname.startsWith("/gallery")
-    ? GalleryPage
-    : LandingPage;
+  // Simple routing
+  const base = import.meta.env.BASE_URL;
+  const fullPath = window.location.pathname;
+  const relativePath = fullPath.startsWith(base)
+    ? fullPath.slice(base.length)
+    : fullPath.replace(/^\//, "");
+
+  const Page = relativePath.startsWith("gallery") ? GalleryPage : LandingPage;
 
   return (
     <>
-      <header style={{ textAlign: "center", margin: "2rem 0" }}>
-        <a
-          href="/"
-          className="
-            text-myflame text-3xl font-bold
-            decoration-myflame underline-offset-4
-            hover:text-myflame/80 select-none
-          "
-        >
-          🏒 BigV Webpage 🏒
-        </a>
-      </header>
-
+      <Header />
       <Page />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style.css";
 
+/**
+ * Bootstrapping react into DOM
+ */
 const container = document.getElementById("root")!;
 createRoot(container).render(
   <React.StrictMode>
