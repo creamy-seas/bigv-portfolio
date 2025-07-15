225a88fb92b94a848f7d7131f21eca3d6b3ed19c
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-07-15 13:44:10 +0100

    feat: succesfull tailwind generation and overview page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,7 @@ module.exports = {
     "./resources/public/index.html",
     "./src/cljs/**/*.{cljs,cljc,html}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        myflame: "var(--color-myflame)",
-        bg:      "var(--color-bg)",
-        fg:      "var(--color-fg)",
-      },
-      spacing: {
-        root: "var(--size-root)",
-      }
-    }
-  },
-  plugins: [
-    require("daisyui"),
-    function ({ addBase, theme }) {
-      addBase({
-        ":root": {
-          "--color-myflame": theme("colors.sandybrown", "sandybrown"),
-          "--color-bg":      theme("colors.gray.900", "#111"),
-          "--color-fg":      theme("colors.gray.200", "#eee"),
-          "--size-root":     "850px",
-        }
-      });
-    }
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: false, // skip and save space
     logs: false,
