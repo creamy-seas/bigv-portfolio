88caf7e0abebf5616204eaec34129f9d28b3c0ba
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-07-16 13:57:45 +0100

    style: tweaking html css to have consistent black background and scrollbar

--- src/clj/pages/gallery/page.clj
+++ src/clj/pages/gallery/page.clj
@@ -1,7 +1,6 @@
 (ns pages.gallery.page
   (:require [utils.date]
             [common.metadata]
-            [clojure.pprint :refer [pprint]]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]
             [hiccup2.core :as h]))
@@ -15,8 +14,13 @@
       (->> rows
            ;; Associate the keys from headers with the row values
            (map (fn [row] (zipmap ks row)))
-           (map (fn [{:keys [date] :as entry}]
-                  (assoc entry :date (utils.date/parse date))))
+           (map (fn [{:keys [date id type] :as entry}]
+                  (assoc entry
+                         :date (utils.date/parse date)
+                         :thumbnail (if (= type "image")
+                                      (str "https://drive.google.com/thumbnail?id=" id)
+                                      "/assets/play-icon.svg")
+                         )))
            (group-by :season)
            ;; Sort within each season
            (map (fn [[season entries]]
@@ -26,23 +30,20 @@
 
 (defn gallery-card
   "Single media element in grid view"
-  [{:keys [id type description date]}]
-  (let [thumb (if (= type "image")
-                (str "/gallery/thumbs/" id ".jpg")
-                "/assets/play-icon.svg")]
-    [:div.cursor-pointer
-     [:imgw.full.h-32.object-cover.rounded-lg {:src thumb :alt description}]
-     [:p.text-sm.text-center.mt-2 description]
-     [:p.text-xs.text-center.text-gray-400
-      (.format (java.time.format.DateTimeFormatter/ofPattern "yyyy-MM-dd") date)]]))
+  [{:keys [thumbnail description date]}]
+  [:div.cursor-pointer
+   [:img.w-full.h-32.object-cover.rounded-lg {:src thumbnail :alt description}]
+   [:p.text-sm.text-center.mt-2 description]
+   [:p.text-xs.text-center.text-gray-400
+    (.format (java.time.format.DateTimeFormatter/ofPattern "yyyy-MM-dd") date)]])
 
 (defn gallery-view
   "Grid of media passed in as argument"
   [season->entries]
-  [:section.container.select-none
+  [:section.container.select-none.space-y-4
    (for [[season items] season->entries]
-     [:div.collapse.collapse-arrow {:key season}
-      [:div {:class "collapse-title text-xl font-semibold bg-myflame/80 text-bg"} season]
+     [:details.collapse.collapse-arrow.rounded-none.rounded-t-lg {:key season}
+      [:summary {:class "collapse-title text-xl font-semibold bg-myflame/80 text-bg"} season]
       [:div.collapse-content.p-2
        [:div.grid.grid-cols-2.sm:grid-cols-3.md:grid-cols-4.gap-4
         (->> items
