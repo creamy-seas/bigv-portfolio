a1235e9998bfb7d6dbfc1c0c391eeaa7ddebd760
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-07-22 21:35:50 +0100

    feat: refactored for more clarity

--- src/cljs/dev/build.clj
+++ src/cljs/dev/build.clj
@@ -3,54 +3,54 @@
             [clojure.java.io  :as io]))
 
 (def bundles
-  {:gallery ["gallery.collapse" "gallery.modal"]
-   :landing ["landing.game-graph"]})
+  ;; Define all the namespaces to builds into js files here
+  ;; e.g. :example 'gallery-build tells to evaluate the gallery.build and put it into example.js
+  {:gallery 'gallery.build
+   :landing 'landing.game-graph
+   })
 
-(def common-opts
-  {:output-dir      "target/cljs/out"
+(def base-opts
+  {:src-dirs        "src/cljs"
+   :output-dir      "target/cljs/out"
    :parallel-build  true
    :target          :none
    :verbose         true})
 
-(defn output-to [bundle-name optimizations]
-  (if (= optimizations :advanced)
-    (str "target/cljs/" bundle-name "_to-bundle.js")
-    (str "resources/public/js/" bundle-name ".js")))
+(defn output-to
+  "Output files based off the bundle name"
+  [bundle optimizations]
+  (let [bundle-name (name bundle)]
+    (if (= optimizations :advanced)
+      (str "target/cljs/" bundle-name "_to-bundle.js")
+      (str "resources/public/js/" bundle-name ".js"))))
 
-(defn make-module
-  "Single module that contains all the sources, writting them to output-to"
-  [bundle-name ns-list optimizations]
-  (let [output-to (output-to bundle-name optimizations)]
-    (io/make-parents output-to)
-    {:entries   (set (map symbol ns-list))
-     :output-to output-to}))
-
-(defn modules-map [optimizations]
+(defn modules-map
+  "Make a module for each of the bundles, specifing namespace to evaluate
+  and where to write the output"
+  [optimizations]
   (into {}
-        (for [[k ns-list] bundles]
-          [(keyword (name k))
-           (make-module (name k) ns-list optimizations)])))
+        (map (fn [[bundle entry-ns]]
+               [(keyword (name bundle))
+                {:entries #{entry-ns}
+                 :output-to (output-to bundle optimizations)}]) bundles)))
+
+(defn build-config
+  "Return the full cljs compiler map"
+  [mode]
+  (let [opts (if (= mode "prod")
+               {:optimizations   :advanced
+                :source-map      false
+                :watch           false}
+               {:optimizations   :simple
+                :source-map      true
+                :watch           true})]
+    (merge base-opts opts {:modules (modules-map (:optimizations opts))})))
 
 (defn -main
   "Pass in `prod` or `dev` build as argument"
-  [& profile]
-  (let [{:keys [optimizations source-map watch] :as opts}
-        (case profile
-          "prod"
-          {:optimizations   :advanced
-           :source-map      false
-           :watch           false}
-          {:optimizations   :simple
-           :source-map      true
-           :watch           true})]
-    (println "-> Building (" optimizations
-             (if watch "with watch" "one-shot")
-             (if source-map "with source maps" "")
-             ")")
-    (let [cfg (merge
-               common-opts
-               opts
-               {:modules (modules-map optimizations)})]
-      (if watch
-        (cljs/watch "src/cljs" cfg)
-        (cljs/build "src/cljs" cfg)))))
+  [& [mode]]
+  (let [cfg (build-config mode)
+        action (if (:watch cfg) cljs/watch cljs/build)]
+    (println "▶ cljs compiling" (:optimizations cfg)
+             (if (:watch cfg) "(watching)" "(building)"))
+    (action (:src-dirs base-opts) cfg)))
