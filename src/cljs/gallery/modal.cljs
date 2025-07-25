a1c2ee1d89fb5da2c439369ee133fb5638990e1d
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-07-25 21:34:23 +0100

    refactor: modal.cljs

--- src/cljs/gallery/modal.cljs
+++ src/cljs/gallery/modal.cljs
@@ -1,21 +1,29 @@
-(ns gallery.modal)
+(ns gallery.modal
+  (:require [utils.dom-operations :refer [get-element-by-id
+                                          get-all
+                                          add-listener
+                                          add-click-listener-by-id]]))
 
-(defonce ^:private modal-state (atom nil))
+(defonce
+  ;; Keep the gallery-idx that should be displayed or `nil` if closed"
+  modal-state (atom nil))
 
-(defn set-gallery-iframe
+(defonce max-gallery-idx*
+  (aget js/window "GALLERY_DATA_MAX_IDX"))
+
+(defn set-gallery-modal-iframe
   "The iframe is populated with supplied values"
   [src description date]
-  (let [iframe (.getElementById js/document "gallery-modal-iframe")]
+  (let [iframe (get-element-by-id "gallery-modal-iframe")
+        date-opts #js {"day"   "numeric"
+                       "month" "long"
+                       "year"  "numeric"}
+        locale (aget js/navigator "language")
+        date-str (.toLocaleDateString (js/Date. date) locale date-opts)]
     (set! (.-src iframe) src)
     (set! (.-title iframe) description)
-    (set! (.-textContent (.getElementById js/document "gallery-modal-description"))
-          description)
-    (let [opts   #js {"day"   "numeric"
-                      "month" "long"
-                      "year"  "numeric"}
-          locale (aget js/navigator "language")
-          date-str (.toLocaleDateString (js/Date. date) locale opts)]
-      (set! (.-textContent (.getElementById js/document "gallery-modal-date")) date-str))))
+    (set! (.-textContent (get-element-by-id "gallery-modal-description")) description)
+    (set! (.-textContent (get-element-by-id "gallery-modal-date")) date-str)))
 
 (defn display-gallery-modal
   "Populate modal with information for item gallery-idx in gallery"
@@ -24,41 +32,34 @@
     (let [data (aget js/window "GALLERY_DATA")
           item (aget data gallery-idx)]
       (reset! modal-state gallery-idx)
-      (set-gallery-iframe
-       ;; Do not touch these - I tried to do aget and they failed, but for data the .-PROP does not work
-       ;; Mysterious
+      ;; Do not touch these - I tried to do aget and they failed, but for data the .-PROP does not work
+      ;; Mysterious
+      (set-gallery-modal-iframe
        (.-src item) (.-description item) (aget item "date"))
-      (.remove (.-classList (.getElementById js/document "gallery-modal")) "hidden"))))
+      (.remove (.-classList (get-element-by-id "gallery-modal")) "hidden"))))
 
 (defn close-gallery-modal [event]
   (.stopPropagation event)
-  (when-let [el (.getElementById js/document "gallery-modal")]
+  (when-let [el (get-element-by-id "gallery-modal")]
     (.add (.-classList el) "hidden")
     (reset! modal-state nil)
-    (set-gallery-iframe nil nil nil)))
+    (set-gallery-modal-iframe nil nil nil)))
 
 (defn open-gallery-modal [event]
   (.stopPropagation event)
-  (display-gallery-modal (js/parseInt (.getAttribute (.-currentTarget event) "gallery-idx"))))
+  (display-gallery-modal
+   (js/parseInt (.getAttribute (.-currentTarget event) "gallery-idx"))))
 
 (defn show-future [event]
   (.stopPropagation event)
-  (display-gallery-modal (max 0 (- @modal-state 1))))
-
-(defonce max-gallery-idx*
-  (aget js/window "GALLERY_DATA_MAX_IDX"))
+  (display-gallery-modal
+   (max 0 (- @modal-state 1))))
 
 (defn show-past [event]
   (.stopPropagation event)
   (display-gallery-modal (min
                           max-gallery-idx*
                           (+ @modal-state 1))))
-
-(defn click-listener
-  "Hook up a listenter to the DOM element found by id with the provided function"
-  [element-id callback]
-  (when-let [el (.getElementById js/document element-id)]
-    (.addEventListener el "click" callback)))bu
 
 (defn handle-keydown [event]
   (when (some? @modal-state)
@@ -68,15 +69,12 @@
       "Escape" (close-gallery-modal event)
       nil)))
 
-(defn mount! []
-  (click-listener "gallery-modal" close-gallery-modal)
-  (click-listener "gallery-modal-future" show-future)
-  (click-listener "gallery-modal-past" show-past)
-  (doseq [el (array-seq (.querySelectorAll js/document ".gallery-card"))]
-    (.addEventListener el "click" open-gallery-modal))
-  (.addEventListener js/document "keydown" handle-keydown))
-
 (defn ^:export init []
-  (mount!))
+  (add-click-listener-by-id "gallery-modal" close-gallery-modal)
+  (add-click-listener-by-id "gallery-modal-future" show-future)
+  (add-click-listener-by-id "gallery-modal-past" show-past)
+  (doseq [el (get-all ".gallery-card")]
+    (add-listener el "click" open-gallery-modal))
+  (add-listener js/document "keydown" handle-keydown))
 
 (init)
