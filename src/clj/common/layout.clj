94aa7c0968e94394978867bc7b968a68af69ead4
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-08-30 07:55:59 +0100

    feat: preload of brother link

--- src/clj/common/layout.clj
+++ src/clj/common/layout.clj
@@ -13,6 +13,12 @@
   [{:keys [title description extra-elements]
     :or   {extra-elements []}}]
   [:head
+   [:link {:rel "preload"
+          :as "image"
+          :href (url/put-on-base "/assets/brother-to-brother.svg")
+          :type "image/svg"
+          :fetchpriority "high"}]
+
    ;; Flip <html> class from no-js -> js ASAP (runs before first paint) if js is enabled
    [:script "(d=>d.classList.replace('no-js','js'))(document.documentElement)"]
 
